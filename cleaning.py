911b8b52318c143dcf8914f56933181cfb0b2c9d
Emil Joensen <emiljoesen@hotmail.com>
2023-05-08 10:35:07 +0200

    Initial commit

---
+++ cleaning.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import pydub
+import os
+
+
+def clean(file: str, newfile: str=None, convert: bool=False):
+
+    data = pd.read_csv(file, sep="\t", header=0, index_col=0)
+
+    csv = []
+    for idx, row in data.iterrows():
+
+        # convert mp3 to wav
+        src = f"clips/{row['path']}"
+        dst = f"audio/{row['path'].replace('.mp3', '.wav')}"
+        if convert:
+            convert_mp3_to_wav(src, dst)
+
+        csv.append(
+            {
+                "id": row["path"].replace(".mp3", ""),
+                "text": row["sentence"]
+            })
+
+    if not newfile:
+        newfile = file.replace(".tsv", ".csv")
+    csv = pd.DataFrame(csv)
+    csv.to_csv(newfile, index=False)
+
+def convert_mp3_to_wav(
+    src: str,
+    dst: str,
+):
+    sound = pydub.AudioSegment.from_mp3(src)
+    sound.export(dst, format="wav")
+
+
+if __name__ == "__main__":
+    os.makedirs("audio", exist_ok=True)
+
+    clean("validated.tsv", "all.csv", False)
+    clean("train.tsv")
+    clean("dev.tsv")
+    clean("test.tsv")
